c2dfe8f5c1dd18398bcb6f4fedba07d0ccdbb622
dark-dream732 <d11416149@gs.takming.edu.tw>
2026-03-17 14:42:45 +0800

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class Test
+{
+	public int Mid;
+	public int Final;
+
+	public double GetAvg()
+	{
+		return (Mid + Final) / 2.0;
+	}
+
+
+	public void SetGrade(int m,int f)
+	{
+		Mid = m; 
+		Final = f;	
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Class_0317_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Test joe, tom, mary;
+
+            joe= new Test();
+            mary= new Test();
+
+            joe.Mid = 80;
+            joe.Final = 90;
+
+            mary.Mid = 60;
+            mary.Final = 66;
+
+            Console.WriteLine("Joe" + joe.GetAvg());
+
+            tom = joe;
+
+            Console.WriteLine("Tom" + joe.GetAvg());
+
+            joe.SetGrade(99, 100);
+
+            Console.WriteLine("Tom" + joe.GetAvg());
+        }
+    }
+}
